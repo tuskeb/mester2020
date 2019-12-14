e003b711d0dffa6063cdf900e9c5315cf9dd5ef8
tanulo <tuskebganz@gmail.com>
2019-12-14 08:32:54 +0100

    Textúra pótlása

--- core/src/hu/csanyzeg/master/Demos/DemoMyGame.java
+++ core/src/hu/csanyzeg/master/Demos/DemoMyGame.java
@@ -1,5 +1,7 @@
 package hu.csanyzeg.master.Demos;
 
+import hu.csanyzeg.master.Demos.Box2dHelper.Box2dScreen;
+import hu.csanyzeg.master.Demos.FlappyBird.FlappyScreen;
 import hu.csanyzeg.master.Demos.LoadingStage.DemoLoadingStage;
 import hu.csanyzeg.master.Demos.Menu.MenuScreen;
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
@@ -10,8 +12,9 @@ public class DemoMyGame extends MyGame {
 	public void create () {
 		super.create();
 		setLoadingStage(new DemoLoadingStage(this));
-		setScreen(new MenuScreen(this));
-		//setScreen(new SzakkorScreen(this));
+		//setScreen(new MenuScreen(this));
+		setScreen(new Box2dScreen(this));
+		//setScreen(new FlappyScreen(this));
 	}
 
 }
