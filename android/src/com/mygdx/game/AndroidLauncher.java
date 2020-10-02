c0fc9b70e3c3dcbc5cca4308b2d9834097076975
tuskeb <tuskebganz@gmail.com>
2020-10-02 16:28:57 +0200

    Kerulgetos 1

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -6,12 +6,14 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 import hu.csanyzeg.master.Demos.DemoMyGame;
+import hu.csanyzeg.master.Kerulgetosjatek.NewGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new DemoMyGame(), config);
+		config.useWakelock = true;
+		initialize(new NewGame(false), config);
 	}
 }
