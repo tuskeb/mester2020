c40d425c09f8311da10ffc353c6f34a639443397
Horváth Dániel <admin@hdani1337.hu>
2020-10-14 19:48:19 +0200

    Start Menü szépítgetés

--- core/src/hu/csanyzeg/master/Demos/Menu/MenuStage.java
+++ core/src/hu/csanyzeg/master/Demos/Menu/MenuStage.java
@@ -35,6 +35,7 @@ import hu.csanyzeg.master.MyBaseClasses.UI.MyLabel;
 import static hu.csanyzeg.master.Demos.Box2dHelper.BoxActor.boxTexture;
 import static hu.csanyzeg.master.Demos.Box2dJoin.ChainLinkActor.linkTexture;
 import static hu.csanyzeg.master.Demos.FlappyBird.BirdActor.birdTexture;
+import static hu.csanyzeg.master.Demos.Menu.Menu.invadersTexture;
 
 /**
  * Created by tuskeb on 2016. 09. 30..
@@ -46,15 +47,10 @@ public class MenuStage extends MyStage {
     public static String kekTexture = "demomenu/kek.jpg";
     public static String startTexture = "demomenu/start.jpg";
     public static String clockTexture = "demomenu/clock.jpg";
-    public static String fireworkTexture = "demomenu/firework.png";
-    public static String startMenuTexture = "demomenu/startmenu.png";
-    public static String powerOffTexture = "demomenu/poweroff.png";
     public static String trebuchet = "demomenu/trebuc.ttf";
     public static String shutdownTexture = "demomenu/shutdown.jpg";
     public static String shutdownSound = "demomenu/shutdown.mp3";
     public static String shutdownWallpaperTexture = "demomenu/shutdownWallpaper.jpg";
-    public static String simpleClockTexture = "demomenu/simpleClock.png";
-    public static String invadersTexture = "spaceinvaders/enemy1.png";
 
     public static AssetList assetList = new AssetList();
     static {
@@ -64,15 +60,11 @@ public class MenuStage extends MyStage {
         assetList.addTexture(startTexture);
         assetList.addTexture(clockTexture);
         assetList.addTexture(linkTexture);
-        assetList.addTexture(fireworkTexture);
         assetList.addTexture(boxTexture);
         assetList.addTextureAtlas(birdTexture);
-        assetList.addTexture(startMenuTexture);
-        assetList.addTexture(powerOffTexture);
         assetList.addTexture(shutdownTexture);
         assetList.addSound(shutdownSound);
         assetList.addTexture(shutdownWallpaperTexture);
-        assetList.addTexture(simpleClockTexture);
         assetList.addTexture(invadersTexture);
         assetList.addFont(trebuchet,trebuchet, 16, Color.WHITE);
     }
@@ -82,32 +74,13 @@ public class MenuStage extends MyStage {
     OneSpriteStaticActor kek;//Tálca
     OneSpriteStaticActor start;//Start menü gombja
     OneSpriteStaticActor clock;//Óra háttere
-    OneSpriteStaticActor startMenu;//Üres Start menü
+    Menu menu;//Start menü
     MyLabel clockLabel;//Óra
-    //-----ASZTAL VÉGE-----
-
-    //-----START MENÜ IKONOK ÉS SZÖVEGEK-----
-    OneSpriteStaticActor linkActor;//Box2D Join ikonja
-    OneSpriteStaticActor fireworkActor;//Firework Demo ikonja
-    OneSpriteStaticActor boxActor;//Box2D Helper ikonja
-    OneSpriteAnimatedActor flappyActor;//Flappy Demo ikonja
-    OneSpriteStaticActor powerOff;//Kikapcsológomb
-    OneSpriteStaticActor simpleActor;//SimpleWorld ikonja
-    OneSpriteStaticActor invaderActor;//SpaceInvaders ikonja
-    MyLabel linkActorLabel;//Box2D Join szövege
-    MyLabel fireworkActorLabel;//Firework Demo szövege
-    MyLabel boxActorLabel;//Box2D Helper szövege
-    MyLabel flappyActorLabel;//Flappy Demo szövege
-    MyLabel simpleActorLabel;//SimpleWorld szövege
-    MyLabel invaderActorLabel;//SpaceInvaders szövege
     OneSpriteStaticActor shutdown;//A Windows leállítása...
     OneSpriteStaticActor shutdownWall;//A kikapcsolóképernyő háttere
-    //-----START MENÜ IKONOK ÉS SZÖVEGEK VÉGE-----
+    //-----ASZTAL VÉGE-----
 
     private boolean show = true;//Szerepeljen e a Start menü és a parancsikonok a képernyőn
-
-    private ArrayList<MyActor> buttons = new ArrayList<>();//Start menü ikonjainak listája
-    private ArrayList<MyLabel> labels = new ArrayList<>();//Start menü szövegeinek listája
 
     public MenuStage(final MyGame game) {
         super(new ResponseViewport(720), game);
@@ -118,7 +91,6 @@ public class MenuStage extends MyStage {
             }
         });
         makeDesktop();//Az asztal definiálása
-        makeButtons();//A Start menü gombjainak (actorok és mellettük lévő labelök) definiálása
         showExplorer();//Az asztal megnyitása
         BootStage.firstBoot = false;//Többször ne hozza be az Üdvözöljük képernyőt
         start.addListener(new ClickListener()
@@ -127,57 +99,18 @@ public class MenuStage extends MyStage {
             public void clicked(InputEvent event, float x, float y) {
                 super.clicked(event, x, y);
                 if(show)
-                {//Ha a Start menü rajt a stagen, akkor levesszük őt, az ikonokat és a szövegeket is
-                    startMenu.remove();
-                    for (MyActor myActor : buttons) myActor.remove();
-                    for (MyLabel myLabel : labels) myLabel.remove();
+                {//Ha a Start menü rajt a stagen, akkor levesszük
+                    menu.remove();
                     show = false;
                 }
                 else
-                {//Ha a Start menü nincs rajt a stagen, akkor visszarakjuk őt, az ikonokat és a szövegeket is
-                    addActor(startMenu);
-                    for (MyActor myActor : buttons) addActor(myActor);
-                    for (MyLabel myLabel : labels) addActor(myLabel);
+                {//Ha a Start menü nincs rajt a stagen, akkor visszarakjuk
+                    addActor(menu);
                     show = true;
                 }
             }
         });
 
-        /**
-         * Ezeket nem adom hozzá a menühöz, mert nem találtam hozzájuk illő ikont, illetve még semmi jelentős nincs bennük
-         *
-        addActor(new MenuButton(game, "Szakkör Demo"){
-            @Override
-            public void init() {
-                super.init();
-                setPosition(0,200);
-                setSize(200,50);
-                addListener(new ClickListener(){
-                    @Override
-                    public void clicked(InputEvent event, float x, float y) {
-                        super.clicked(event, x, y);
-                        game.setScreen(new SzakkorScreen(game));
-                    }
-                });
-            }
-        });
-
-
-        addActor(new MenuButton(game, "Actor Demo"){
-            @Override
-            public void init() {
-                super.init();
-                setPosition(0,100);
-                setSize(200,50);
-                addListener(new ClickListener(){
-                    @Override
-                    public void clicked(InputEvent event, float x, float y) {
-                        super.clicked(event, x, y);
-                        game.setScreen(new ActorScreen(game));
-                    }
-                });
-            }
-        });*/
     }
 
     private void shutdown()
@@ -213,7 +146,7 @@ public class MenuStage extends MyStage {
             }
         };
 
-        startMenu = new OneSpriteStaticActor(game, startMenuTexture);
+        menu = new Menu(game);
         bliss = new OneSpriteStaticActor(game, blissTexture);
         kek = new OneSpriteStaticActor(game, kekTexture);
         start = new OneSpriteStaticActor(game, startTexture);
@@ -228,8 +161,7 @@ public class MenuStage extends MyStage {
         clockLabel.setAlignment(0);
         clockLabel.setX(clock.getX() + clock.getWidth()/2 - clockLabel.getWidth()/2);
         clockLabel.setY(clock.getY() + clock.getHeight()/2 - clockLabel.getHeight()/2);
-        startMenu.setPosition(0,kek.getY()+kek.getHeight());
-        startMenu.setSize(startMenu.getWidth()*1.3f, startMenu.getHeight()*1.3f);
+        menu.setPosition(0,kek.getY()+kek.getHeight());
     }
 
     private void showExplorer()
@@ -249,29 +181,22 @@ public class MenuStage extends MyStage {
 
         //Még fél másodperc múlva jön a Start menü és az ikonok
         addTimer(new TickTimer(1, false, new TickTimerListener() {
-
-
             @Override
             public void onTick(Timer sender, float correction) {
-                addActor(startMenu);
-                for (MyActor myActor : buttons) addActor(myActor);
-                for (MyLabel myLabel : labels) addActor(myLabel);
+                addActor(menu);
+                for (MenuItemType type : MenuItemType.values())
+                    menu.addItem(type);
             }
-
         }));
-
     }
 
-    private void closeExplorer()
+    public void closeExplorer()
     {
         //Először a Start menüt távolítjuk el
         addTimer(new TickTimer(0.5f, false, new TickTimerListener() {
-
             @Override
             public void onTick(Timer sender, float correction) {
-                startMenu.remove();
-                for (MyActor myActor : buttons) myActor.remove();
-                for (MyLabel myLabel : labels) myLabel.remove();
+                menu.remove();
             }
         }));
 
@@ -297,233 +222,6 @@ public class MenuStage extends MyStage {
             }
 
         }));
-    }
-
-    ClickListener linkListener;//Box2D Join listener
-    ClickListener fireworkListener;//Firework Demo listener
-    ClickListener boxListener;//Box2D Helper listener
-    ClickListener flappyListener;//Flappy Demo listener
-    ClickListener clockListener;//SimpleWorld listener
-    ClickListener invaderListener;//SpaceInvader listener
-
-    private void makeButtons()
-    {
-        //-----BOX2D JOIN-----
-        linkListener = new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                game.setScreenWithPreloadAssets(Box2dJoinScreen.class, new DemoPreLoadingStage(game));
-            }
-        };
-
-        linkActor = new OneSpriteStaticActor(game, linkTexture){
-            @Override
-            public void init() {
-                super.init();
-                setPosition(7,startMenu.getY() + 80);
-                setSize(32*1.3f,(32/getWidth()*getHeight())*1.3f);
-                addListener(linkListener);
-                buttons.add(this);
-            }
-        };
-            linkActorLabel = new MyLabel(game, "B2D Join Demo", getLabelStyle()) {
-                @Override
-                public void init() {
-                    setPosition(55, linkActor.getY() - linkActor.getHeight()/2 + 8);
-                    setFontScale(1.5f);
-                    setColor(Color.BLACK);
-                    addListener(linkListener);
-                    labels.add(this);
-                }
-            };
-        //----------
-
-
-        //-----FIREWORK DEMO-----
-        fireworkListener = new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                game.setScreen(new FireworkScreen(game));
-            }
-        };
-
-
-        fireworkActor = new OneSpriteStaticActor(game, fireworkTexture){
-            @Override
-            public void init() {
-                super.init();
-                setPosition(linkActor.getX(),linkActor.getY() + linkActor.getHeight() + 25);
-                setSize(32*1.3f,(32/getWidth()*getHeight())*1.3f);
-                addListener(fireworkListener);
-                buttons.add(this);
-            }
-        };
-
-            fireworkActorLabel = new MyLabel(game, "Firework Demo", getLabelStyle()) {
-                @Override
-                public void init() {
-                    setPosition(55, fireworkActor.getY() + 8);
-                    setColor(Color.BLACK);
-                    setFontScale(1.5f);
-                    addListener(fireworkListener);
-                    labels.add(this);
-                }
-            };
-        //----------
-
-
-        //-----BOX2D HELPER-----
-        boxListener = new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                game.setScreenWithPreloadAssets(Box2dHelperScreen.class, new DemoPreLoadingStage(game));
-            }
-        };
-
-        boxActor = new OneSpriteStaticActor(game, boxTexture){
-            @Override
-            public void init() {
-                super.init();
-                setPosition(linkActor.getX(),fireworkActor.getY() + fireworkActor.getHeight() + 15);
-                setSize(32*1.3f,(32/getWidth()*getHeight())*1.3f);
-                addListener(boxListener);
-                buttons.add(this);
-            }
-        };
-
-            boxActorLabel = new MyLabel(game, "Box2D Helper", getLabelStyle()) {
-                @Override
-                public void init() {
-                    setPosition(55, boxActor.getY() + 10);
-                    setColor(Color.BLACK);
-                    setFontScale(1.5f);
-                    addListener(boxListener);
-                    labels.add(this);
-                }
-            };
-        //----------
-
-
-        //-----FLAPPY DEMO-----
-        flappyListener = new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                game.setScreenWithPreloadAssets(FlappyScreen.class, new DemoPreLoadingStage(game));
-            }
-        };
-
-        flappyActor = new OneSpriteAnimatedActor(game, birdTexture){
-            @Override
-            public void init() {
-                super.init();
-                setPosition(linkActor.getX(),boxActor.getY() + boxActor.getHeight() + 20);
-                setFps(6);
-                addListener(flappyListener);
-                buttons.add(this);
-            }
-        };
-
-            flappyActorLabel = new MyLabel(game, "Flappy Demo", getLabelStyle()) {
-                @Override
-                public void init() {
-                    setPosition(55, flappyActor.getY()+1);
-                    setColor(Color.BLACK);
-                    setFontScale(1.5f);
-                    addListener(flappyListener);
-                    labels.add(this);
-                }
-            };
-        //----------
-
-
-        //-----SIMPLE WORLD-----
-        clockListener = new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                game.setScreen(new SWScreen(game));
-            }
-        };
-
-        simpleActor = new OneSpriteStaticActor(game, simpleClockTexture){
-            @Override
-            public void init() {
-                super.init();
-                setPosition(linkActor.getX(),flappyActor.getY() + flappyActor.getHeight() + 15);
-                setSize(32*1.3f,(32/getWidth()*getHeight())*1.3f);
-                addListener(clockListener);
-                buttons.add(this);
-            }
-        };
-
-            simpleActorLabel = new MyLabel(game, "Simple Clock", getLabelStyle()) {
-                @Override
-                public void init() {
-                    setPosition(55, simpleActor.getY() + 10);
-                    setColor(Color.BLACK);
-                    setFontScale(1.5f);
-                    addListener(clockListener);
-                    labels.add(this);
-                }
-            };
-        //----------
-
-
-        //-----SPACE INVADERS-----
-        invaderListener = new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                game.setScreen(new SpaceScreen(game));
-            }
-        };
-
-        invaderActor = new OneSpriteStaticActor(game, invadersTexture){
-            @Override
-            public void init() {
-                super.init();
-                setPosition(linkActor.getX(),simpleActor.getY() + simpleActor.getHeight() + 15);
-                setSize(32*1.3f,(32/getWidth()*getHeight())*1.3f);
-                addListener(invaderListener);
-                buttons.add(this);
-            }
-        };
-
-        invaderActorLabel = new MyLabel(game, "Space Invaders", getLabelStyle()) {
-            @Override
-            public void init() {
-                setPosition(55, invaderActor.getY() + 10);
-                setColor(Color.BLACK);
-                setFontScale(1.5f);
-                addListener(invaderListener);
-                labels.add(this);
-            }
-        };
-        //----------
-
-
-        //-----KIKAPCSOLÓGOMB-----
-        powerOff = new OneSpriteStaticActor(game, powerOffTexture){
-            @Override
-            public void init() {
-                super.init();
-                setSize(getWidth()*1.3f,getHeight()*1.3f);
-                setPosition(startMenu.getX() + startMenu.getWidth() - getWidth(), startMenu.getY());
-                addListener(new ClickListener(){
-                    @Override
-                    public void clicked(InputEvent event, float x, float y) {
-                        super.clicked(event, x, y);
-                        closeExplorer();
-                    }
-                });
-                buttons.add(this);
-            }
-        };
-        //----------
     }
 
     private Label.LabelStyle getLabelStyle()
